bf7ccd0822608fa75c9b0f6e686a0c13cb1b1854
Eren Telimen <etelimen@uncc.edu>
2017-10-10 18:02:02 +0000

    Done with section 4.1.

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -22,20 +22,21 @@ class MoviesController < ApplicationController
   # will render app/views/movies/show.html.haml by default
   end
 
-
   def create
-    @movie = Movie.create!(params[:movie])
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-
+  
   def edit
     @movie = Movie.find params[:id]
   end
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
